a89841708f55ce2f3ff3f70cf23e9e9178b30954
liangjiamin <liangjiamin.011235@bytedance.com>
2023-04-11 17:30:46 +0800

    remove apps

--- packages/dev-config/eslint-react-preset.js
+++ packages/dev-config/eslint-react-preset.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: ["next", "./eslint-base-preset.js"],
   settings: {
     next: {
-      rootDir: ["apps/*/", "packages/*/"],
+      rootDir: ["packages/*/"],
     },
   },
 };
